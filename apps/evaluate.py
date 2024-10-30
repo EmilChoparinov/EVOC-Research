b8c1f39244eb03802f1dc89e62024017ec28364d
EmilChoparinov <emilchoparinov@gmail.com>
2024-10-30 10:29:36 +0100

    upload visualization script

--- apps/evaluate.py
+++ apps/evaluate.py
@@ -8,7 +8,6 @@ genotype requested `PARAMS`.
 
 from revolve2.simulators.mujoco_simulator import LocalSimulator
 
-import config
 import numpy as np
 from revolve2.standards.simulation_parameters import make_standard_batch_parameters
 from revolve2.experimentation.logging import setup_logging
@@ -28,16 +27,21 @@ from revolve2.modular_robot import ModularRobot
 from revolve2.modular_robot.body.base import ActiveHinge, Body
 from revolve2.modular_robot.brain.cpg import BrainCpgNetworkStatic, CpgNetworkStructure
 from revolve2.standards import fitness_functions, terrains
+from revolve2.standards.modular_robots_v2 import gecko_v2
 
 PARAMS = np.array(
-    [-1.99028127,  0.08435464,  0.00391817, -0.02174904,  1.94509952,
-        1.60096773,  1.61155222, -1.99356261, -1.12994603]
+   [-2.99999447,  1.81308186, -0.0331997 ,  0.06852362,  2.89306811,
+       -0.58302268,  2.76108739, -2.99385889, -1.80114772]
+    # [-1.99028127,  0.08435464,  0.00391817, -0.02174904,  1.94509952,
+    #     1.60096773,  1.61155222, -1.99356261, -1.12994603]
 )
 
 def main() -> None:
     setup_logging()
 
-    active_hinges = config.BODY.find_modules_of_type(ActiveHinge)
+    body = gecko_v2()
+
+    active_hinges = body.find_modules_of_type(ActiveHinge)
 
     (
         cpg_network_structure,
@@ -45,7 +49,7 @@ def main() -> None:
     ) = active_hinges_to_cpg_network_structure_neighbor(active_hinges)
     
     robot = ModularRobot(
-                body=config.BODY,
+                body=body,
                 brain=BrainCpgNetworkStatic.uniform_from_params(
                     params=PARAMS,
                     cpg_network_structure=cpg_network_structure,

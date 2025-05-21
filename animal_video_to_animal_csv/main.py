e2d0d777507cdd1b41885ff7653335facfd4c05c
vlad <vmnechita@yahoo.com>
2025-05-21 21:07:22 +0200

    animal_data

--- animal_video_to_animal_csv/main.py
+++ animal_video_to_animal_csv/main.py
@@ -14,7 +14,7 @@ HIJKL
 """
 labels = [chr(ord('A') + i) for i in range(13)]
 headers = ['Frame'] + labels
-for i in range(16, 64):
+for i in range(0, 64):
     frame = cv2.imread(f"./Files/Frames/frame_{i}.png")
     points = get_points_from_frame(frame)
 
